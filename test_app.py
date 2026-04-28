649e0a53e17365490fc918dfe64f9d198ac7f913
Seu Nome <seuemail@email.com>
2026-04-27 21:39:36 -0300

    Adicionando 5 testes unitários e configuração de CI

---
+++ test_app.py
@@ -0,0 +1,29 @@
+import unittest
+
+def soma(a, b):
+    return a + b
+
+def saudacao(nome):
+    return f"Olá, {nome}!"
+
+def inverter_texto(texto):
+    return texto[::-1]
+
+class TestProjetoDevops(unittest.TestCase):
+    def test_soma(self):
+        self.assertEqual(soma(2, 3), 5)
+
+    def test_soma_negativa(self):
+        self.assertEqual(soma(-1, -1), -2)
+
+    def test_saudacao(self):
+        self.assertEqual(saudacao("Camilly"), "Olá, Camilly!")
+
+    def test_inverter(self):
+        self.assertEqual(inverter_texto("DevOps"), "spOvDe")
+
+    def test_texto_diferente(self):
+        self.assertNotEqual(inverter_texto("Docker"), "Docker")
+
+if __name__ == '__main__':
+    unittest.main()
